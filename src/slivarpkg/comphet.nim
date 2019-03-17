e77bb0ce117e81e9178409a6c217d8311e6b30b1
Brent Pedersen <bpederse@gmail.com>
2019-03-17 11:47:44 -0600

    support compound het with 1 end is a de novo

--- src/slivarpkg/comphet.nim
+++ src/slivarpkg/comphet.nim
@@ -6,14 +6,37 @@ import argparse
 import tables
 import sets
 
+template inherited(kid: pedfile.Sample, a:openarray[int8]): bool =
+  # this avoids checks as they are already done in is_compound het
+  a[kid.i] == 1 and (a[kid.mom.i] == 1 or a[kid.dad.i] == 1)
+
+template denovo(kid: pedfile.Sample, a:openarray[int8]): bool =
+  # this avoids checks as they are already done in is_compound het
+  a[kid.i] == 1 and (a[kid.mom.i] == 0 and a[kid.dad.i] == 0)
+
+proc compound_denovo(kid: pedfile.Sample, a:openarray[int8], b:openarray[int8]): bool {.inline.} =
+  ## this should only be callled from is_compount_het as it avoids re-checking stuff already in there.
+  ## it checks if there is transmitted het and 1
+  if kid.denovo(a) and kid.inherited(b): return true
+  if kid.denovo(b) and kid.inherited(a): return true
+  return false
+
 proc is_compound_het*(kid: pedfile.Sample, a:openarray[int8], b:openarray[int8]): bool {.inline.} =
+  # check if the kid (with mom and dad) has either a classic compound het
+  # where each parent transmits 1 het to the kid or where 1 het is transmitted
+  # and the other is de novo.
   doAssert kid.dad != nil and kid.mom != nil
   if a[kid.i] != 1 or b[kid.i] != 1: return false
 
   if a[kid.dad.i] == -1 or a[kid.mom.i] == -1: return false
+  if b[kid.dad.i] == -1 or b[kid.mom.i] == -1: return false
+
+  # if only a single het between the parents, only possiblity is a de novo.
+  if a[kid.dad.i] + a[kid.mom.i] + b[kid.dad.i] + b[kid.mom.i] == 1:
+    return compound_denovo(kid, a, b)
+
   if a[kid.dad.i] + a[kid.mom.i] != 1: return false
 
-  if b[kid.dad.i] == -1 or b[kid.mom.i] == -1: return false
   if b[kid.dad.i] + b[kid.mom.i] != 1: return false
   # now we have 1 het parent at each site and kid het at both sites.
   # check that different parents have the different alleles.
@@ -153,12 +176,9 @@ when isMainModule:
   import unittest
   suite "compound hets":
     test "simple":
-      var kid = Sample(id:"kid")
-      kid.mom = Sample(id:"mom")
-      kid.dad = Sample(id:"dad")
-      kid.i = 0
-      kid.dad.i = 1
-      kid.mom.i = 2
+      var kid = Sample(id:"kid", i:0)
+      kid.dad = Sample(id:"dad", i: 1)
+      kid.mom = Sample(id:"mom", i: 2)
 
       check is_compound_het(kid, [1'i8, 0, 1], [1'i8, 1, 0])
 
@@ -166,3 +186,14 @@ when isMainModule:
       check not is_compound_het(kid, [1'i8, 1, 1], [1'i8, 0, 1])
 
       check not is_compound_het(kid, [0'i8, 0, 1], [1'i8, 1, 0])
+
+    test "with denovo":
+      var kid = Sample(id:"kid", i:0)
+      kid.dad = Sample(id:"dad", i: 1)
+      kid.mom = Sample(id:"mom", i: 2)
+
+      # first is de novo, 2nd is inherited
+      check is_compound_het(kid, [1'i8, 0, 0], [1'i8, 1, 0])
+      check not is_compound_het(kid, [1'i8, 1, 0], [1'i8, 1, 0])
+
+      check is_compound_het(kid, [1'i8, 0, 1], [1'i8, 0, 0])
