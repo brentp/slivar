446e3df8bb23458e7546cb5daa8860015347bf8f
Brent Pedersen <bpederse@gmail.com>
2019-04-15 15:15:05 -0600

    better upd filtering

--- js/my.js
+++ js/my.js
@@ -23,8 +23,25 @@ function x_denovo(kid, mom, dad) {
   return ((mom.AD[1] + dad.AD[1]) < 2);
 }
 
+function hq_ab(sample) {
+  // make sure the allele balance is close to 0 for homref, 1 for homalt
+  // and intermediate for het
+  if(sample.alts == 0){
+    return sample.AB < 0.01
+  }
+  if(sample.alts == 1){
+    return sample.AB > 0.3 && sample.AB < 0.7
+  }
+  if(sample.alts == 2){
+    return sample.AB > 0.99
+  }
+  return false
+}
+
 function uniparent_disomy(kid, mom, dad) {
-  if(!hq(kid, mom, dad)){ return false; }
+  if(kid.DP < 10 || mom.DP < 10 || dad.DP < 10) { return false; }
+  if(kid.GQ < 20 || mom.GQ < 20 || dad.GQ < 20) { return false; }
+  if(!(hq_ab(kid) && hq_ab(mom) && hq_ab(dad))){ return false; }
   return (kid.alts == 0 || kid.alts == 2) && ((mom.alts == 2 && dad.alts == 0) || (mom.alts == 0 && dad.alts == 2));
 }
 
@@ -33,8 +50,8 @@ function recessive(kid, mom, dad) {
 }
 
 function x_recessive(kid, mom, dad) {
-  return (hq(kid, mom, dad) && mom.alts == 1 && dad.alts == 0 && kid.alts == 1
-              && kid.sex == 'male' && mom.AB > 0.2 && mom.AB < 0.8 && kid.AB > 0.3)
+  return (hq(kid, mom, dad) && mom.alts == 1 && dad.alts == 0 && kid.alts >= 1
+              && kid.sex == 'male' && mom.AB > 0.25 && mom.AB < 0.75 && kid.AB > 0.75)
 }
 
 function lenient_denovo(kid, mom, dad){
