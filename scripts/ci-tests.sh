286c93fdfbcbca98c5e62d1253d45e23a98005ae
Brent Pedersen <bpederse@gmail.com>
2019-06-11 20:12:34 -0600

    add functional tests to CI

--- scripts/ci-tests.sh
+++ scripts/ci-tests.sh
@@ -6,4 +6,5 @@ git clone --depth 1 https://github.com/samtools/htslib
 git clone --depth 1 https://github.com/samtools/bcftools
 cd bcftools && make -j4 install && cd ../.. && rm -rf test-tmp
 nimble install -y && nimble test
+bash tests/functional-tests.sh
 nim c -d:release -o:/test/slivar_static -d:nsb_static src/slivar
