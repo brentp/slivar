6775ebc7d788f4b6105c9a670b3478e680b69397
Brent Pedersen <bpederse@gmail.com>
2019-07-19 16:47:02 -0600

    paper stuff

--- paper/exomes.sh
+++ paper/exomes.sh
@@ -31,20 +31,35 @@ python paper/plot_ab_roc.py roc-gq{0,5,10}.txt
 exit
 DONE
 
+bcf=vcfs/$cohort.annotated.bcf
 
 <<DENOVO
-bcf=vcfs/$cohort.annotated.bcf
+export SLIVAR_SUMMARY_FILE=$cohort.dn.summary.tsv
 
 slivar expr --vcf $bcf --ped $ped \
     --pass-only \
     -o vcfs/$cohort.dn.bcf \
     --info "variant.FILTER == 'PASS' && variant.ALT[0] != '*'" \
-    --trio "dn_ab_gq:mom.alts == 0 && dad.alts == 0 && kid.alts == 1 && kid.GQ >= 5 && mom.GQ >= 5 && dad.GQ >= 5 && kid.AB >= 0.2 && kid.AB <= 0.8" \
-    --trio "dn:mom.alts == 0 && dad.alts == 0 && kid.alts == 1 && kid.GQ >= 5 && mom.GQ >= 5 && dad.GQ >= 5 && kid.AB >= 0.2 && kid.AB <= 0.8 && INFO.gnomad_popmax_af < 0.001 && !('gnomad_popmax_af_filter' in INFO) && INFO.topmed_af < 0.01" 
+    --trio "dn_ab_gq:mom.hom_ref && dad.hom_ref && kid.het && kid.GQ >= 5 && mom.GQ >= 5 && dad.GQ >= 5 && kid.AB >= 0.2 && kid.AB <= 0.8" \
+    --trio "dn:mom.hom_ref && dad.hom_ref && kid.het && kid.GQ >= 5 && mom.GQ >= 5 && dad.GQ >= 5 && kid.AB >= 0.2 && kid.AB <= 0.8 && INFO.gnomad_popmax_af < 0.001 && !('gnomad_popmax_af_filter' in INFO) && INFO.topmed_af < 0.01"  \
 
-python paper/plot-exome-dn-summary.py paper/exome.summary.tsv
+python plot-exome-dn-summary.py $cohort.dn.summary.tsv
 
+exit
 DENOVO
 
-#bcftools csq -s - --ncsq 40 -g $gff -l -f $fasta vcfs/$cohort.bcf -O u \
-#  | slivar compound-hets -f BCSQ -i 2 --sample-field AR_filtered --sample-field denovo -p $ped > vcfs/$cohort.ch.vcf
+export SLIVAR_SUMMARY_FILE=$cohort.summary.tsv
+# now get plot of total variant counts:
+slivar expr --vcf $bcf --ped $ped \
+    --pass-only \
+    --js paper.js \
+    --info "variant.FILTER == 'PASS' && variant.ALT[0] != '*' && INFO.gnomad_popmax_af < 0.01 && INFO.topmed_af < 0.02" \
+    --trio "denovo:mom.hom_ref && dad.hom_ref && kid.het && kid.GQ >= 5 && mom.GQ >= 5 && dad.GQ >= 5 && kid.AB >= 0.2 && kid.AB <= 0.8 && INFO.gnomad_popmax_af < 0.001 && !('gnomad_popmax_af_filter' in INFO) && INFO.topmed_af < 0.01" \
+    --trio "recessive:recessive(kid, mom, dad)" \
+    --trio "x_denovo:x_denovo(kid, mom, dad) && (variant.CHROM == 'chrX' || variant.CHROM == 'X')" \
+    --trio "x_recessive:x_recessive(kid, mom, dad) && (variant.CHROM == 'chrX' || variant.CHROM == 'X')" \
+    --trio "comphet_side:comphet_side(kid, mom, dad) && INFO.gnomad_nhomalt_controls < 10" \
+    | bcftools csq -s - --ncsq 40 -g $gff -l -f $fasta - -o vcfs/$cohort.vcf
+
+export SLIVAR_SUMMARY_FILE=$cohort.ch.summary.tsv
+slivar compound-hets -f BCSQ  --sample-field comphet_side --sample-field denovo -p $ped -v vcfs/$cohort.vcf > vcfs/$cohort.ch.vcf
