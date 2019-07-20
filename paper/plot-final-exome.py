0fbe7c57cfe9485cd254e416ec4d6016262785c8
Brent Pedersen <bpederse@gmail.com>
2019-07-20 08:26:37 -0600

    subplot for autosomal dominant

--- paper/plot-final-exome.py
+++ paper/plot-final-exome.py
@@ -10,26 +10,31 @@ df = pd.read_csv(sys.argv[1], sep="\t", index_col=0)
 dfo = df.join(pd.read_csv(sys.argv[2], sep="\t", index_col=0))
 dfo.drop("comphet_side", inplace=True, axis="columns")
 
+ad = dfo.auto_dom
+ad = pd.DataFrame({"auto_dom": ad})
+
+dfo.drop("auto_dom", inplace=True, axis="columns")
 
 df = dfo.melt(value_name="number_of_variants")
 
 
-
-fig, axes = plt.subplots(1, figsize=(7, 7))
-axes = [axes]
+fig, axes = plt.subplots(1, 2, figsize=(7, 7), gridspec_kw={'width_ratios': [3, 1]})
+#axes = [axes]
 sns.swarmplot(x="variable", y="number_of_variants",
        data=df, ax=axes[0], palette=colors)
 
-axes[0].set_xlabel("Inheritance mode")
-axes[0].set_ylabel("Candidate variants")
-#axes[0].set_xticklabels(["0.2 <= AB < 0.8\n& GQ >= 5",
-#                         "gnomAD popmax AF < 0.001\n& PASS in gnomAD\n & topMed AF < 0.01\n"])
+print(ad.head())
 
+sns.swarmplot(y="auto_dom", data=ad, ax=axes[1],
+        palette=colors)
+
+axes[1].set_ylabel("")
+axes[0].set_xlabel("Inheritance mode", horizontalalignment='left')
+axes[1].set_xlabel("")
+#plt.xlabel("Inheritance mode")
+axes[0].set_ylabel("Candidate variants")
 
 ax = axes[0]
-#ax.set_ylim(-0.5, 14.5)
-#ax.axhline(o, 0.1, 0.4, lw=4)
-#ax.axhline(f, 1-0.4, 1 - 0.1, lw=4)
 
 labels = ax.get_xticklabels()
 lookups = {"denovo":"de novo", "compound-het":"compound-heterozygote",
@@ -37,12 +42,10 @@ lookups = {"denovo":"de novo", "compound-het":"compound-heterozygote",
         "auto_dom":"autosomal dominant",
         "x_denovo": "x-linked de novo"}
 labels = [lookups[l.get_text()] for l in labels]
-ax.set_xticklabels(labels, rotation=10)
+
+ax.set_xticklabels(labels, rotation=12)
+axes[1].set_xticklabels(["autosomal dominant"], rotation=12)
 
 plt.tight_layout()
 sns.despine()
 plt.show()
-
-
-
-
