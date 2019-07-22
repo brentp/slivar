65f0ebf6a331dfdeda14756c803e6ef3a1070613
Brent Pedersen <bpederse@gmail.com>
2019-07-22 10:48:05 -0600

    plot both in a single plot

--- paper/plot_ab_roc.py
+++ paper/plot_ab_roc.py
@@ -2,39 +2,35 @@ import sys
 
 from matplotlib import pyplot as plt
 import seaborn as sns
+sns.set_style("white")
 import pandas as pd
 
 colors = sns.color_palette("RdYlBu", 13)
 colors = sns.cubehelix_palette(7, start=2, rot=0, dark=0.05, light=.95, reverse=False)
 colors = sns.color_palette("Set1", 13)
 
-df = pd.read_csv(sys.argv[1], sep="\t")
-cols = list(df.columns)
-cols[0] = cols[0].lstrip('#')
-df.columns = cols
+df_exome = pd.read_csv(sys.argv[1], sep="\t")
+df_genome = pd.read_csv(sys.argv[2], sep="\t")
 
-if "genome" in sys.argv[1]:
-    df = df[df.exclude == "exclude"]
-else:
-    df = df[df.exclude == "."]
-tmax = float(df.total.max())
+for df in [df_exome, df_genome]:
+    cols = list(df.columns)
+    cols[0] = cols[0].lstrip('#')
+    df.columns = cols
 
-fig, axes = plt.subplots(len(df.GQ.unique()), 1, sharex=True, sharey=True, figsize=(5, 12))
 
-gqs = sorted(df.GQ.unique())
+fig, axes = plt.subplots(len(df.GQ.unique()), 2, figsize=(8, 12))
+assert len(df_exome.GQ.unique()) == len(df_genome.GQ.unique())
 
-for i, gq in enumerate(gqs):
-    df_gq = df[df.GQ == gq]
-    ax = axes[i]
+for ci, df in enumerate((df_exome, df_genome)):
+    tmax = float(df.total.max())
+    gqs = sorted(df.GQ.unique())
 
-    # each df has exclude and '' (for no exclusion)
-    for k, sel in enumerate(("exclude", ".")):
-
-        dfs = df_gq[df_gq.exclude == sel]
+    for i, gq in enumerate(gqs):
+        df_gq = df[df.GQ == gq]
+        ax = axes[i, ci]
+        dfs = df_gq
         if len(dfs) == 0: continue
         dfs.tp = dfs.tp * (dfs.total / tmax)
-
-        #if k == 0: continue
 
         cutoffs = [0.1, 0.15, 0.2, 0.25, 0.3]
         ax.plot(dfs.fp, dfs.tp, color=(0.6, 0.6, 0.6))
@@ -47,25 +43,17 @@ for i, gq in enumerate(gqs):
             ax.plot([cut.fp], [cut.tp], color=colors[j+1], ls='none',
                     marker='o', label="%.2g-%.2g" % (cutoff, 1 - cutoff))
 
-        """
-        if not "genome" in sys.argv[1] and gqs[i] == 5:
-            cutoff = dfs[dfs.cutoff > 0.2].iloc[0, :]
-
-            ax.annotate('0.2 <= AB < 0.8', xy=(cutoff.fp, cutoff.tp),
-                    xytext=(cutoff.fp+0.0003, cutoff.tp-0.03),
-                arrowprops=dict(facecolor='gray', shrink=0.02),
-                )
-        elif "genome" in sys.argv[1] and gqs[i] == 10:
-            cutoff = dfs[dfs.cutoff > 0.22].iloc[0, :]
-
-            ax.annotate('0.25 <= AB < 0.75', xy=(cutoff.fp, cutoff.tp),
-                    xytext=(cutoff.fp+0.0003, cutoff.tp-0.03),
-                arrowprops=dict(facecolor='gray', shrink=0.02),
-                )
-        """
+            if ci == 0 and cutoff == 0.2 and gq == 10:
+                ax.plot([cut.fp], [cut.tp], color='gray', ls='none',
+                        markersize=12,
+                        marker='o', zorder=-1)
+            elif ci == 1 and cutoff == 0.25 and gq == 10:
+                ax.plot([cut.fp], [cut.tp], color='gray', ls='none',
+                        markersize=12,
+                        marker='o', zorder=-1)
 
 
-        if not "genome" in sys.argv[1]:
+        if ci == 0:
             ax.set_xlim(0.0, 0.0024)
             ax.set_ylim(0.8, 1)
         else:
@@ -73,12 +61,17 @@ for i, gq in enumerate(gqs):
             ax.set_ylim(0.5, 1)
 
         sns.despine()
-        if i == 0:
+        if i == 0 and ci == 0:
             ax.legend(title="Allele-balance cut-offs")
         ax.set_title("Genotype-quality cutoff: %d" % gqs[i])
         ax.set_ylabel("Transmission rate")
 
-axes[len(axes)-1].set_xlabel("Mendelian-violation rate")
+axes[len(axes)-1, 0].set_xlabel("Mendelian-violation rate")
+axes[len(axes)-1, 1].set_xlabel("Mendelian-violation rate")
+
+axes[0, 0].set_title("Exome", fontsize=15)
+axes[0, 1].set_title("Genome", fontsize=15)
+
 #plt.ylim(0.8, 1)
 #sns.despine()
 ax = axes[0]
