8619c9a72b99b59003e4b664a084263bd3070cc8
Brent Pedersen <bpederse@gmail.com>
2019-10-21 16:30:40 -0600

    fix ci

--- scripts/ci-tests.sh
+++ scripts/ci-tests.sh
@@ -2,8 +2,9 @@
 set -e
 apk add -q bash
 mkdir test-tmp && cd test-tmp
-git clone --depth 1 https://github.com/samtools/htslib
-git clone --depth 1 https://github.com/samtools/bcftools
-cd bcftools && make -j4 install && cd ../.. && rm -rf test-tmp
+git clone https://github.com/samtools/htslib
+cd htslib && git checkout 1.9  && cd ..
+git clone https://github.com/samtools/bcftools
+cd bcftools && git checkout 1.9 && make -j4 install && cd ../.. && rm -rf test-tmp
 nimble install -y && nimble test
 nim c -d:danger -o:/test/slivar_static -d:nsb_static src/slivar
