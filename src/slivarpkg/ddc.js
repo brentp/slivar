a9b7754196f4db10197a4f4033694e63260cc7b3
Brent Pedersen <bpederse@gmail.com>
2019-11-18 12:29:01 -0700

    working flip and abs and text hover

--- src/slivarpkg/ddc.js
+++ src/slivarpkg/ddc.js
@@ -12,28 +12,49 @@ function wNumb(opts) {
     };
 }
 
-function roc(values, violations, invert, abs) {
+function roc(values, violations, invert, abs, name, filters_to_keep) {
+    let filters = variant_infos.filters
     var A = values.map(function(val, i) {
         if(invert) {val = -val};
         if(abs){val = Math.abs(val)};
         return [val, violations[i]]
+    }).filter(function(val, i) {
+        return filters_to_keep.includes(filters[i])
     })
     A.sort(function(a, b) {
         return a[0] - b[0]
     })
-    result = {x:[], y:[]}
+    result = {x:[0], y:[0], text: [""]}
     var tps = 0
     var fps = 0
     let N = A.length
+    if(N == 0) {
+        return result
+    }
+    var txt = (abs ? "Math.abs(": "") + "INFO." + name + (abs ? ") ": " ") + (invert ? ">" : "<")
+    // we can't just iterate over A, we also have to track the change in
+    // cutoff so we don't draw a smooth curve when the cutoff value hasn't
+    // changed.
+    var last_val = A[0][0] - 1;
     A.forEach(function(pair, i) {
         // FP
         if(pair[1]) { fps += 1; }
         else {tps += 1; }
+        var val = pair[0]
+        if (val == last_val) { return; }
+        last_val = val;
+
         // TODO: scale by A.length
         // but might be good to leave as numbers for now.
         result.x.push(fps)
         result.y.push(tps)
+        result.text.push(`${txt} ${val} gives FDR: ${(fps/(tps + fps)).toFixed(3)}`)
     })
+    result.x.push(fps)
+    result.y.push(tps)
+    result.text.push(`${txt} ${A[A.length-1][0]} gives FDR: ${(fps/(tps + fps)).toFixed(3)}`)
+
+
     return result
 }
 
@@ -41,9 +62,9 @@ function add_slider(values, name, label) {
 
     jQuery('#variant-sliders').append(`<hr>
 <label for=${name}-flip>flip values</label>
-<input type="checkbox" id=${name}-flip name=${name}-flip>
+<input type="checkbox" class=slivar-checkbox id=${name}-flip name=${name}-flip>
 <label for=${name}-abs>take absolute value</label>
-<input type="checkbox" id=${name}-abs name=${name}-abs>
+<input type="checkbox" class=slivar-checkbox id=${name}-abs name=${name}-abs>
 
 <div id=${name}-slider name=${name}-slider></div>
 <label for="${name}-slider"><h3>${label}</h3></label>
@@ -55,17 +76,83 @@ function add_slider(values, name, label) {
     var vlmin = Math.min.apply(null, values);
     var vlmax = Math.max.apply(null, values);
     sliders[`${name}`] = noUiSlider.create(document.getElementById(`${name}-slider`), {
-    tooltips: [wNumb({decimals: 3}), wNumb({decimals: 3})],
-    start: [vlmin, vlmax],
-    connect: true, 
-    range: {min:vlmin, max:vlmax},
+        tooltips: [wNumb({decimals: 3}), wNumb({decimals: 3})],
+        start: [vlmin, vlmax],
+        connect: true, 
+        range: {min:vlmin, max:vlmax},
+    })
+    plot_info(values, name, label)
 
-})
+    jQuery(`#${name}-abs, #${name}-flip`).on('change', function() {
+        plot_info(values, name, label);
+    });
+
+}
+
+function get_passing_info_idxs() {
+    var info_ranges = {}
+    var info_bools = {} // TODO
+    for(k in sliders) {
+        if(k == "samples") { continue; }
+        var tmp = sliders[k].get()
+        info_ranges[k] = [parseInt(tmp[0]), parseInt(tmp[1])]
+    }
+    var filters_to_keep = jQuery('.slivar-filters:checked').map(function(v) { return this.name }).toArray();
+
+    var variant_lengths_ranges = info_ranges['variant-length']
+    delete info_ranges['variant-length'];
+
+    var idxs = []
+    for(var i = 0; i < variant_infos.variant_lengths.length; i++){
+        var vl = variant_infos.variant_lengths[i];
+        if(vl < variant_lengths_ranges[0] || vl > variant_lengths_ranges[1]) { continue;}
+        var skip = false;
+        for(k in info_ranges){
+            var vf = variant_infos.float_tbl[k][i];
+            if (vf < info_ranges[k][0] || vf > info_ranges[k][1]){
+                skip = true;
+                break;
+            }
+        }
+        if(!skip) { idxs.push(i) }
+    }
+    return idxs
+}
+
+function main_plot() {
+    // main-roc-plot
+    console.time('main-plot')
+    var traces = [];
+    // we only plot points that pass the INFO filters
+    let idxs = new Set(get_passing_info_idxs())
+    console.log(idxs.length)
+    trios.forEach(function(trio) {
+        var trace = {x:[], y:[], text:[], name: trio.sample_id}
+        for(var i=0; i < trio.variant_idxs.length; i++) {
+            if(!idxs.has(trio.variant_idxs[i])){ continue; }
+            trace.x.push(1)
+            trace.y.push(1)
+
+
+        }
+        traces.push(trace)
+
+    })
+    console.timeEnd('main-plot')
+}
+
+function plot_info(values, name, label) {
+    var vlmin = Math.min.apply(null, values);
+    var vlmax = Math.max.apply(null, values);
+    var filters_to_keep = jQuery('.slivar-filters:checked').map(function(v) { return this.name }).toArray();
+
     var bin_size = (vlmax - vlmin) / 100;
     var inh_vals = []
     var vio_vals = []
     var vios = variant_infos.violations.slice();
+    let filters = variant_infos.filters;
     for(var k=0; k < values.length; k++){
+        if(!filters_to_keep.includes(filters[k])) { continue; }
         if(vios[k]) {
             vio_vals.push(values[k])
         } else {
@@ -78,10 +165,10 @@ function add_slider(values, name, label) {
         legend: {x: 0.9, y: 0.9},
          
     };
-
-    var do_flip = jQuery(`#{name}-flip`).is(":checked")
-    var do_abs = jQuery(`#{name}-abs`).is(":checked")
-    var roc_tr = roc(values, vios, do_flip, do_abs);
+    var do_flip = jQuery(`#${name}-flip`).is(":checked")
+    // TODO: remove absolute value. not very helpful.
+    var do_abs = jQuery(`#${name}-abs`).is(":checked")
+    var roc_tr = roc(values, vios, do_flip, do_abs, name, filters_to_keep);
 
     var traces = [
         {type: 'histogram', x: inh_vals, name:"transmitted", autobinx: false, xbins: {size: bin_size}, histnorm: "count"},
@@ -93,8 +180,8 @@ function add_slider(values, name, label) {
         legend: {x: 0.9, y: 0.9},
     }
 
-    plots.hists[`${name}`] = Plotly.newPlot(`${name}-hist-plot`, traces, layout);
-    plots.rocs[`${name}`] = Plotly.newPlot(`${name}-roc-plot`, [roc_tr], roc_layout);
+    plots.hists[`${name}`] = Plotly.newPlot(`${name}-hist-plot`, traces, layout, {displayModeBar: false});
+    plots.rocs[`${name}`] = Plotly.newPlot(`${name}-roc-plot`, [roc_tr], roc_layout, {displayModeBar: false});
 }
 
 function add_sample_slider(values, name) {
@@ -124,8 +211,11 @@ for(k in seen) { nseen += 1; if(k == "PASS"){ haspass = true; }}
 if(nseen > 0) {
     for(k in seen){
     var checked = ((!haspass) || (k == "PASS") ? "checked": "");
-        jQuery('#filter-column').append(`<input type='checkbox' ${checked} name=${k} value=${k} id=${k}>${k}<br>`)
+        jQuery('#filter-column').append(`<input type='checkbox' class=slivar-filters ${checked} name=${k} value=${k} id=${k}-check >${k}<br>`)
     }
+    jQuery('.slivar-filters').on('change', function() {
+        jQuery('.slivar-checkbox').trigger('change');
+    });
 }
 
 add_slider(variant_infos.variant_lengths, "variant-length", "variant lengths")
@@ -142,61 +232,7 @@ for(k in trios[0].kid_tbl) {
 }
 
 
-
+main_plot()
 
 }());
-
-var colors = ['rgba(55,126,184,0.7)', 'rgba(228,26,28,0.7)', 'rgba(77,175,74,0.7)', 'rgba(152,78,163,0.7)', 'rgba(255,127,0,0.7)', 'rgba(166,86,40,0.7)', 'rgba(247,129,191,0.7)']
-
-var layout = {
-    autosize: true,
-    margin: {t: 30, pad: 0},
-    xaxis: {
-        title: "Mendelian violations",
-        showspikes: true,
-        spikethickness: 1,
-    hoverformat: '.4f',
-    },
-    yaxis: {
-        title: "Transmitted variants",
-        showspikes: true,
-        spikethickness: 1,
-    hoverformat: '.4f',
-    },
-    hovermode: 'closest',
-    showlegend: true,
-    legend: {
-        xanchor: "right",
-        yanchor: "bottom",
-        y: 0.1,
-        x: 1,
-        orientation: "h",
-        borderwidth: 1,
-        bordercolor: '#eeeeee'
-    },
-}
-
-var layout = {
-    autosize: true,
-    margin: {t: 30, pad: 0},
-    xaxis: {
-        title: "Variant size-class",
-        hoverformat: '.3f',
-    },
-    yaxis: {
-        title: "False discovery rate",
-        hoverformat: '.3f',
-    },
-    hovermode: 'closest',
-    showlegend: true,
-    legend: {
-        x: 0.4,
-        y: 0.9,
-        borderwidth: 1,
-        bordercolor: '#eeeeee'
-    },
-
-}
-
-
 
