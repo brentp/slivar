ee04b3ad5ac4fb69d622c0f2cf6ab00828d93bd6
Brent Pedersen <bpederse@gmail.com>
2020-01-16 15:40:39 -0700

    code for figure3

--- paper/plot-final-exome.py
+++ paper/plot-final-exome.py
@@ -5,53 +5,122 @@ from matplotlib import pyplot as plt
 
 import sys
 
-colors = sns.color_palette("Set1", 12)
+colors2 = sns.color_palette("Set1", 12)[3:]
 
 colors = [(0.6, 0.6, 0.6)] * 12
 
-df = pd.read_csv(sys.argv[1], sep="\t", index_col=0)
-dfo = df.join(pd.read_csv(sys.argv[2], sep="\t", index_col=0))
-dfo.drop("comphet_side", inplace=True, axis="columns")
 
-ad = dfo.auto_dom
-ad = pd.DataFrame({"auto_dom": ad})
+def read_dfs(i, j):
 
-dfo.drop("auto_dom", inplace=True, axis="columns")
+    df = pd.read_csv(sys.argv[i], sep="\t", index_col=0)
+    dfo = df.join(pd.read_csv(sys.argv[j], sep="\t", index_col=0))
+    dfo.drop("comphet_side", inplace=True, axis="columns")
+    ad = dfo.auto_dom
+    ad = pd.DataFrame({"number_of_variants": ad})
+    dfo.drop("auto_dom", inplace=True, axis="columns")
+    df = dfo.melt(value_name="number_of_variants")
+    df.number_of_variants += np.random.random(size=df.shape[0]) / 2.0
+    return df, ad
 
-df = dfo.melt(value_name="number_of_variants")
+def get_mean_df(df_means, order):
 
-df.number_of_variants += np.random.random(size=df.shape[0]) / 2.5
-size=3
+    df_means = df_means.to_dict(orient="dict")["number_of_variants"]
+
+    df_means_o = [df_means[(l, "all")] for l in order]
+    df_means_i = [df_means[(l, "impactful")] for l in order]
+
+    df_means_b = [[labels[i], o, "all"] for i, o in enumerate(df_means_o)] + \
+                 [[labels[i], o, "impactful"] for i, o in enumerate(df_means_i)]
+
+    df_means_bar = pd.DataFrame(df_means_b, columns=["inheritance mode", "mean number of variants",
+        "variant class"])
+    return df_means_bar
 
 
-fig, axes = plt.subplots(1, 2, figsize=(7, 7), gridspec_kw={'width_ratios': [3, 1]})
-#axes = [axes]
-sns.swarmplot(x="variable", y="number_of_variants",
-       data=df, ax=axes[0], palette=colors, size=size)
+df, ad = read_dfs(1, 2)
 
-print(ad.head())
 
-sns.swarmplot(y="auto_dom", data=ad, ax=axes[1],
-        palette=colors, size=size)
 
-axes[1].set_ylabel("")
-axes[0].set_xlabel("Inheritance mode", horizontalalignment='left')
-axes[1].set_xlabel("")
+dfi, adi = read_dfs(3, 4)
+
+df["cat"] = "all"
+dfi["cat"] = "impactful"
+
+ad["cat"] = "all"
+adi["cat"] = "impactful"
+
+df = pd.concat((df, dfi))
+ad = pd.concat((ad, adi), ignore_index=True)
+ad["variable"] = "autosomal dominant"
+
+print(df.head())
+#print(ad.head())
+
+
+df_means = df.groupby(["variable", "cat"]).mean()
+ad_means = ad.groupby(["variable", "cat"]).mean()
+print(df_means, ad_means)
+
+
+size = 2
+
+fig, axes = plt.subplots(2, 2, figsize=(8, 5), gridspec_kw={'width_ratios': [5,
+    1], 'height_ratios': [1, 3]})
+
+sns.swarmplot(x="variable", y="number_of_variants", hue="cat", dodge=True,
+       data=df, ax=axes[1,0], palette=colors2, size=size)
+
+sns.swarmplot(x="variable", y="number_of_variants", data=ad, ax=axes[1,1], hue="cat", dodge=True,
+        palette=colors2, size=size)
+
+axes[1,1].set_ylabel("")
+axes[1,0].set_xlabel("Inheritance mode", horizontalalignment='left')
+axes[1,1].set_xlabel("")
 #plt.xlabel("Inheritance mode")
-axes[0].set_ylabel("Candidate variants")
+axes[1,0].set_ylabel("Candidate variants")
 
-ax = axes[0]
+ax = axes[1,0]
 
 labels = ax.get_xticklabels()
 lookups = {"denovo":"de novo", "compound-het":"compound-heterozygote",
         "x_recessive":"x-linked recessive", "recessive": "autosomal recessive",
         "auto_dom":"autosomal dominant",
         "x_denovo": "x-linked de novo"}
+order = [l.get_text() for l in labels]
 labels = [lookups[l.get_text()] for l in labels]
 
 ax.set_xticklabels(labels, rotation=12)
-axes[1].set_xticklabels(["autosomal dominant"], rotation=12)
+axes[1,1].set_xticklabels(["autosomal dominant"], rotation=12)
+
+axes[1,1].get_legend().remove()
+axes[1,0].get_legend().set_title("variant class")
+
+
+df_means_bar = get_mean_df(df_means, order)
+
+
+sns.barplot(data=df_means_bar, hue="variant class", x="inheritance mode",
+        y="mean number of variants", ax=axes[0, 0], palette=colors2)
+axes[0, 0].set_ylabel(None)
+axes[0, 0].set_xlabel(None)
+axes[0, 0].set_xticklabels([])
+axes[0, 0].set_title("mean number of candidate variants per trio")
+axes[0, 0].get_legend().remove()
+
+
+order = ["autosomal dominant"]
+ad_means_bar = get_mean_df(ad_means, ["autosomal dominant"])
+
+
+sns.barplot(data=ad_means_bar, hue="variant class", x="inheritance mode",
+        y="mean number of variants", ax=axes[0, 1], palette=colors2)
+axes[0, 1].set_ylabel(None)
+axes[0, 1].set_xlabel(None)
+axes[0, 1].set_xticklabels([])
+axes[0, 1].get_legend().remove()
+
 
 plt.tight_layout()
 sns.despine()
+plt.savefig("figure3-exome-counts.png")
 plt.show()
