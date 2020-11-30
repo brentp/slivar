0c6d5b1bd612c320038a611cdf3ec1619c41d21c
Brent Pedersen <bpederse@gmail.com>
2020-11-30 10:15:32 -0700

    comment out unused

--- paper/giab/get.sh
+++ paper/giab/get.sh
@@ -4,13 +4,11 @@ wget -qc ftp://ftp-trace.ncbi.nlm.nih.gov/giab/ftp/release/AshkenazimTrio/HG002_
 
 set -euo pipefail
 
-ref=/data/human/g1k_v37_decoy.fa
 ref=/data/human/Homo_sapiens_assembly38.fasta
 PATH=$PATH:~/src/rtg-tools/dist/rtg-tools-3.11-39691f9f/
 sdf=hg38.sdf
 #see: https://precision.fda.gov/challenges/10/view
 
-#rtg format -o hg37.sdf /data/human/g1k_v37_decoy.fa
 if [[ -z $sdf ]]; then
 	rtg format -o $sdf $ref
 fi
@@ -29,7 +27,7 @@ rm -rf eval
 rtg vcfeval --bed-regions $bed -b $truth -o eval -t $sdf -c $vcf
 
 python ab-truth.py eval/fp.vcf.gz eval/tp.vcf.gz eval/fn.vcf.gz
-#<<DONE
+<<DONE
 bcftools view --threads 3 -g "het" $vcf -O z -o het.vcf.gz
 bcftools index --threads 3 --tbi het.vcf.gz
 
@@ -38,7 +36,6 @@ bcftools index --threads 3 --tbi gq20dp10.vcf.gz
 
 bcftools view --threads 3 -g "het" $vcf -O z -o bench.vcf.gz
 bcftools index --threads 3 --tbi bench.vcf.gz
-#DONE
 
 rm -rf eval-het
 rm -rf eval-gq20dp10
@@ -47,3 +44,4 @@ rtg vcfeval --bed-regions $bed -b bench.vcf.gz -o eval-het -t $sdf -c het.vcf.gz
 rtg vcfeval --bed-regions $bed -b bench.vcf.gz -o eval-gq20dp10 -t $sdf -c gq20dp10.vcf.gz
 
 
+DONE
