a04aa8a39c7f9aa69e5cbac30a19620061635a6f
Andrew Kane <andrew@ankane.org>
2026-05-12 13:55:59 -0400

    Improve version message when compiled from release archive (#186)

--- src/slivarpkg/version.nim
+++ src/slivarpkg/version.nim
@@ -1,2 +1,4 @@
+import strutils
+
 const slivarVersion* = "0.3.4"
-const slivarGitCommit* = staticExec("git rev-parse --verify HEAD")
+const slivarGitCommit* = staticExec("git rev-parse --verify HEAD 2>/dev/null").strip()
